2905c02842af2e6ebf47a7d1400ec285bb99bb34
Doni Romdoni <58791550+donikalong@users.noreply.github.com>
2022-03-26 21:33:26 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Menampilkan File PDF Ke Halaman Website - www.bangkoding.com</title>
+</head>
+<body>
 
+	<h4>Menampilkan file pdf menggunakan tag embed</h4>
+	<embed type="application/pdf" src="contoh.pdf" width="600" height="400"></embed>
+
+	
+
+	<h4>Menampilkan file pdf menggunakan tag iframe</h4>
+	<iframe src="config.php" width="600" height="400"></iframe>
+
+    <h4>Menampilkan file pdf menggunakan tag object</h4>
+	<object data="contoh.pdf" width="600" height="400"></object>
+
+</body>
+</html>
